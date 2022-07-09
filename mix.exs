4ee0d7becf0e651582ec7dc1f398925f31b41640
sdnoel <44347551+sdnoel@users.noreply.github.com>
2022-07-09 16:34:45 -0400

    initial commit

---
+++ mix.exs
@@ -0,0 +1,23 @@
+# Copyright 2022 Agile Data, Inc <code@mftlabs.io>
+
+defmodule AmpsCore.MixProject do
+
+  use Mix.Project
+
+  def project do
+    [
+      app: :amps_core,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      aliases: aliases(),
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: []
+    ]
+  end
+
+
+end
