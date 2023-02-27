afdbb23124961087bebf1d17459c4947b8ffadf5
aram0112 <48899851+aram0112@users.noreply.github.com>
2023-02-26 19:12:40 -0600

    Update application.ex

--- lib/application.ex
+++ lib/application.ex
@@ -1,24 +1,24 @@
-defmodule AmpsCore.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
+# defmodule AmpsCore.Application do
+#   # See https://hexdocs.pm/elixir/Application.html
+#   # for more information on OTP Applications
+#   @moduledoc false
 
-  use Application
+#   use Application
 
-  @impl true
-  def start(_type, _args) do
-    children = [
-      {
-        Mnesiac.Supervisor,
-        [
-          [name: Amps.MnesiacSupervisor]
-        ]
-      }
-    ]
+#   @impl true
+#   def start(_type, _args) do
+#     children = [
+#       {
+#         Mnesiac.Supervisor,
+#         [
+#           [name: Amps.MnesiacSupervisor]
+#         ]
+#       }
+#     ]
 
-    Supervisor.start_link(children,
-      strategy: :one_for_one,
-      name: AmpsCore.Supervisor
-    )
-  end
-end
+#     Supervisor.start_link(children,
+#       strategy: :one_for_one,
+#       name: AmpsCore.Supervisor
+#     )
+#   end
+# end
